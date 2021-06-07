63e2497ccb4baffa93f19a964ebcb57e61950c3e
lkmSasanga <lkmsasanga@gmail.com>
2021-06-07 22:58:28 +0530

    meetupDetails component added

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,14 @@
-import { Fragment } from "react";
+
+import MeetupDetail from "../../components/meetups/MeetupDetails";
 
 function MeetupDetails() {
   return (
-    <Fragment>
-      <img
-        src="https://images.unsplash.com/photo-1558536219-f17a76e52621?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1028&q=80"
-        alt="A First Meetup"
-      />
-      <h1> A First Meetup </h1> <address> Some Street 5, Some City </address>{" "}
-      <p> The meetup description. </p>{" "}
-    </Fragment>
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1558536219-f17a76e52621?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1028&q=80"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
   );
 }
 
