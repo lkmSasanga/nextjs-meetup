3dcca939f89a089883405706de7b0ace1d3d725e
lkmSasanga <lkmsasanga@gmail.com>
2021-06-11 08:40:47 +0530

    getStaticProps added to meetupID

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,34 @@
-
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
-  return (
-    <MeetupDetail
-      image="https://images.unsplash.com/photo-1558536219-f17a76e52621?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1028&q=80"
-      title="First Meetup"
-      address="Some Street 5, Some City"
-      description="This is a first meetup"
-    />
-  );
+    return ( <
+        MeetupDetail image = "https://images.unsplash.com/photo-1558536219-f17a76e52621?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1028&q=80"
+        title = "First Meetup"
+        address = "Some Street 5, Some City"
+        description = "This is a first meetup" /
+        >
+    );
+}
+
+export async function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupData: {
+                image: "https://images.unsplash.com/photo-1558536219-f17a76e52621?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1028&q=80",
+                id: meetupId,
+                title: "First Meetup",
+                address: "Some Street 5, Some City",
+                description: "This is a first meetup"
+            }
+        }
+    }
+
 }
 
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -26,32 +26,32 @@ function HomePage(props) {
     />;
 }
 
-export async function getServerSideProps(context) {
-    // always run after thte deployment in server side
-    // runs every request happens
+// export async function getServerSideProps(context) {
+//     // always run after thte deployment in server side
+//     // runs every request happens
 
-    const req = context.req;
-    const res = context.res;
+//     const req = context.req;
+//     const res = context.res;
 
-    // fetch data from an API
+//     // fetch data from an API
 
-    return {
-        meetups: DUMMY_MEETUPS
-    };
-}
-
-
-// export async function getStaticProps() {
-//     // never execute in the client side
-//     // build on the server
-//     // can do fetch data from API
 //     return {
-//         props: {
-//             meetups: DUMMY_MEETUPS
-//         },
-//         revalidate: 1
+//         meetups: DUMMY_MEETUPS
 //     };
 // }
+
+
+export async function getStaticProps() {
+    // never execute in the client side
+    // build on the server
+    // can do fetch data from API
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        },
+        revalidate: 1
+    };
+}
 
 
 
